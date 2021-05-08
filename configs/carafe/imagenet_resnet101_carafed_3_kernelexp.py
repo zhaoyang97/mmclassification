f5ed28892d269213c0bdd64c00c6727e1e3a2d52
zhaoyang97 <1173201671@qq.com>
2021-05-09 01:23:28 +0800

    增加resnet101的实验

---
+++ configs/carafe/imagenet_resnet101_carafed_3_kernelexp.py
@@ -0,0 +1,13 @@
+_base_ = [
+    '../_base_/models/resnet101.py', '../_base_/datasets/i_imagenet_bs16.py',
+    '../_base_/schedules/imagenet_bs128.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        # type='ResNet',
+        # type='ResNet_carafed',
+        type='ResNet_carafed_3_kernelexp',
+    )
+)
